6948c31fe6b66cd3553b4c132006097e885eb72d
varax <varax@DESKTOP-CH496DU>
2021-11-15 22:55:38 +0200

    Добавьте файлы проекта.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.тестуванняToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вхідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.реєстраціяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.результатToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отриманняДанихToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проДодатокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.тестуванняToolStripMenuItem,
+            this.результатToolStripMenuItem,
+            this.проДодатокToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(713, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // тестуванняToolStripMenuItem
+            // 
+            this.тестуванняToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.вхідToolStripMenuItem,
+            this.реєстраціяToolStripMenuItem,
+            this.вихідToolStripMenuItem});
+            this.тестуванняToolStripMenuItem.Name = "тестуванняToolStripMenuItem";
+            this.тестуванняToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.тестуванняToolStripMenuItem.Text = "Тестування";
+            // 
+            // вхідToolStripMenuItem
+            // 
+            this.вхідToolStripMenuItem.Name = "вхідToolStripMenuItem";
+            this.вхідToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.вхідToolStripMenuItem.Text = "Вхід";
+            this.вхідToolStripMenuItem.Click += new System.EventHandler(this.вхідToolStripMenuItem_Click);
+            // 
+            // реєстраціяToolStripMenuItem
+            // 
+            this.реєстраціяToolStripMenuItem.Name = "реєстраціяToolStripMenuItem";
+            this.реєстраціяToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.реєстраціяToolStripMenuItem.Text = "Реєстрація";
+            this.реєстраціяToolStripMenuItem.Click += new System.EventHandler(this.реєстраціяToolStripMenuItem_Click);
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.вихідToolStripMenuItem.Text = "Вихід";
+            this.вихідToolStripMenuItem.Click += new System.EventHandler(this.вихідToolStripMenuItem_Click);
+            // 
+            // результатToolStripMenuItem
+            // 
+            this.результатToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отриманняДанихToolStripMenuItem});
+            this.результатToolStripMenuItem.Name = "результатToolStripMenuItem";
+            this.результатToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.результатToolStripMenuItem.Text = "Результат";
+            // 
+            // отриманняДанихToolStripMenuItem
+            // 
+            this.отриманняДанихToolStripMenuItem.Name = "отриманняДанихToolStripMenuItem";
+            this.отриманняДанихToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.отриманняДанихToolStripMenuItem.Text = "Отримання даних";
+            this.отриманняДанихToolStripMenuItem.Click += new System.EventHandler(this.отриманняДанихToolStripMenuItem_Click);
+            // 
+            // проДодатокToolStripMenuItem
+            // 
+            this.проДодатокToolStripMenuItem.Name = "проДодатокToolStripMenuItem";
+            this.проДодатокToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.проДодатокToolStripMenuItem.Text = "Про додаток";
+            this.проДодатокToolStripMenuItem.Click += new System.EventHandler(this.проДодатокToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(713, 422);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MainWindow";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem тестуванняToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вхідToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem реєстраціяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem результатToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem проДодатокToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem отриманняДанихToolStripMenuItem;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        TextBox txtBox;
+        TextBox txtBox1;
+        TextBox txtBox2;
+        TextBox txtBoxName;
+        TextBox txtBoxAge;
+        Label lbl;
+        Button btnlog;
+        Button btnConf;
+        Button btnreg;
+        DataGridView data;
+        public string[,] mass = { { "adm", "adm", "Admin", "21" }, { "kik", "parol", "Kick", "22" }, { "varax", "rur", "Varax", "12" }, { "jojo", "top", "Joeseph", "16" }, { "kick", "butovski", "Slava", "13" } };
+        public string[,] mass1 = { {"","", "", ""} };
+        public int rows;
+        public int colums;
+        public bool q = true;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void вхідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            txtBox = new TextBox();
+            txtBox.Location = new Point(300, 50);
+            txtBox.Visible = true;
+            panel1.Controls.Add(txtBox);
+            txtBox1 = new TextBox();
+            txtBox1.Location = new Point(300, 100);
+            txtBox1.Visible = true;
+            panel1.Controls.Add(txtBox1);
+            lbl = new Label();
+            lbl.Location = new Point(100, 50);
+            lbl.Visible = true;
+            lbl.Text = "Login";
+            panel1.Controls.Add(lbl);
+            lbl = new Label();
+            lbl.Location = new Point(100, 100);
+            lbl.Visible = true;
+            lbl.Text = "Password";
+            panel1.Controls.Add(lbl);
+            btnlog = new Button();
+            btnlog.Location = new Point(150, 150);
+            btnlog.Visible = true;
+            btnlog.Text = "Log in";
+            panel1.Controls.Add(btnlog);
+            btnlog.Click += new EventHandler(btnlog_Click);
+        }
+        private void реєстраціяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            txtBox = new TextBox();
+            txtBox.Location = new Point(300, 50);
+            txtBox.Visible = true;
+            panel1.Controls.Add(txtBox);
+            txtBox1 = new TextBox();
+            txtBox1.Location = new Point(300, 100);
+            txtBox1.Visible = true;
+            panel1.Controls.Add(txtBox1);
+            txtBox2 = new TextBox();
+            txtBox2.Location = new Point(300, 150);
+            txtBox2.Visible = true;
+            panel1.Controls.Add(txtBox2);
+            lbl = new Label();
+            lbl.Location = new Point(100, 50);
+            lbl.Visible = true;
+            lbl.Text = "Login";
+            panel1.Controls.Add(lbl);
+            lbl = new Label();
+            lbl.Location = new Point(100, 100);
+            lbl.Visible = true;
+            lbl.Text = "Password";
+            panel1.Controls.Add(lbl);
+            lbl = new Label();
+            lbl.Width = 2000;
+            lbl.Location = new Point(100, 150);
+            lbl.Visible = true;
+            lbl.Text = "Confirm password";
+            panel1.Controls.Add(lbl);
+            btnreg = new Button();
+            btnreg.Location = new Point(200, 200);
+            btnreg.Visible = true;
+            btnreg.Text = "Sign up";
+            panel1.Controls.Add(btnreg);
+            btnreg.Click += new EventHandler(btnreg_Click);
+        }
+
+        private void btnreg_Click(object sender, EventArgs e)
+        {
+            bool a = true;
+            rows = mass.GetUpperBound(0) + 1;
+            for (int i = 0; i < rows; i++)
+            {
+                
+                if (txtBox.Text != mass[i, 0] && txtBox1.Text == txtBox2.Text)
+                {
+                    q = false;
+                    a = false;
+                    mass1[0, 0] = txtBox.Text;
+                    mass1[0, 1] = txtBox1.Text;
+                   
+                }
+                
+            }
+            if (a)
+            {
+                MessageBox.Show("Error");
+            }
+            else
+            {
+                panel1.Controls.Clear();
+                lbl = new Label();
+                lbl.Location = new Point(100, 50);
+                lbl.Visible = true;
+                lbl.Text = "Enter ur name";
+                panel1.Controls.Add(lbl);
+                lbl = new Label();
+                lbl.Location = new Point(100, 100);
+                lbl.Visible = true;
+                lbl.Text = "Enter ur age";
+                panel1.Controls.Add(lbl);
+                txtBoxName = new TextBox();
+                txtBoxName.Location = new Point(300, 50);
+                txtBoxName.Visible = true;
+                panel1.Controls.Add(txtBoxName);
+                txtBoxAge = new TextBox();
+                txtBoxAge.Location = new Point(300, 100);
+                txtBoxAge.Visible = true;
+                panel1.Controls.Add(txtBoxAge);
+                btnConf = new Button();
+                btnConf.Location = new Point(200, 200);
+                btnConf.Visible = true;
+                btnConf.Text = "Sign up";
+                panel1.Controls.Add(btnConf);
+                btnConf.Click += new EventHandler(btnConf_Click);
+            
+            }
+        }
+        private void btnConf_Click(object sender, EventArgs e)
+        {
+            mass1[0, 2] = txtBoxName.Text;
+            mass1[0, 3] = txtBoxAge.Text;
+            отриманняДанихToolStripMenuItem_Click(null, null);
+        }
+        private void btnlog_Click(object sender, EventArgs e)
+        {
+            bool a = true;
+            rows = mass.GetUpperBound(0) + 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                
+
+                    if (txtBox.Text == mass[i, 0] && txtBox1.Text == mass[i, 1])
+                    {
+                        a = false;
+                        отриманняДанихToolStripMenuItem_Click(null, null);
+                        break;
+                    }
+
+
+                
+            }
+            if (a)
+            {
+                MessageBox.Show("Incorrect login/password. Try again!");
+            }
+        }
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void отриманняДанихToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            data = new DataGridView();
+            data.Location = new Point(100, 50);
+            data.Visible = true;
+            data.Width = 500;
+            data.Height = 300;
+            data.Columns.Add("Index", "Index");
+            data.Columns.Add("Value", "Users");
+            data.Columns.Add("Value1", "Name");
+            data.Columns.Add("Value2", "Age");
+            int n = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                n = i + 2;
+                data.Rows.Add(new object[] { i + 1, mass[i,0], mass[i, 2], mass[i,  3]});   
+            }
+            if (q == false)
+            {
+                data.Rows.Add(new object[] { n, mass1[0, 0], mass1[0, 2], mass1[0, 3] });
+            }
+            panel1.Controls.Add(data);
+        }
+
+        private void проДодатокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            lbl = new Label();
+            lbl.Width = 2000;
+            lbl.Location = new Point(100, 150);
+            lbl.Visible = true;
+            lbl.Text = "App created by.... just created";
+            panel1.Controls.Add(lbl);
+        }
+    }
+}
+
